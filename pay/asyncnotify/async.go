1d94e61a7744c522598724491c191c6adeabd349
houseme <housemecn@gmail.com>
2023-11-22 21:46:06 +0800

    feat: improve code for check sign

--- pay/asyncnotify/async.go
+++ pay/asyncnotify/async.go
@@ -53,7 +53,7 @@ func (a *AsyncNotify) AsyncNotify(ctx context.Context, req *AsyncRequest) (resp 
     }
     
     var checkSignResult bool
-    if checkSignResult, err = helper.CheckSign(req.ByteTimestamp, req.ByteNonceStr, req.Msg, req.ByteSignature, a.ctxCfg.Config.PublicKey()); err != nil {
+    if checkSignResult, err = helper.CheckSign(req.ByteTimestamp, req.ByteNonceStr, req.Content, req.ByteSignature, a.ctxCfg.Config.PublicKey()); err != nil {
         resp.ErrNo = ErrNoSystemError
         resp.ErrTips = ErrTipsSystemError
         return
@@ -62,6 +62,7 @@ func (a *AsyncNotify) AsyncNotify(ctx context.Context, req *AsyncRequest) (resp 
     if !checkSignResult {
         resp.ErrNo = ErrNoFailedToCheckTheSignature
         resp.ErrTips = "failed"
+        return
     }
     if req.Type == AsyncPay {
         var data = new(PaymentData)
